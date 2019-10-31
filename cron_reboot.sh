1a71bd4f6dc738dbd693c8eb4541e7ad14421c59
root <root@tor001.janxb.de>
2019-10-31 14:26:08 +0100

    fixed logging and tor service reload after config change

--- cron_reboot.sh
+++ cron_reboot.sh
@@ -3,8 +3,10 @@ set -eu
 
 DIR=/root/tor-node-config
 
-do-package-upgrades -y >> /var/log/tor/package-upgrades.log
+do-package-upgrades -y &>> /var/log/tor/package-upgrades.log
 
-git -C $DIR pull >> /var/log/git-pull.log
+git -C $DIR pull &>> /var/log/tor/git-pull.log
 
 $DIR/generate_tor_config.sh
+
+service tor reload
