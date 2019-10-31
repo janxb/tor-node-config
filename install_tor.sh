85d540168e2b5db4da16f37fce4c685d056ead7b
root <root@tor001.janxb.de>
2019-10-31 14:18:54 +0100

    improved install script

--- install_tor.sh
+++ install_tor.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -eu
 
+if [ ! -d "/usr/local/sbin" ]; then
+ git clone https://github.com/janxb/serverutils /usr/local/sbin
+fi
+
 echo "deb https://deb.torproject.org/torproject.org bionic main" >/etc/apt/sources.list.d/tor.list
 
 apt-key-download https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
 
 apt update
+
+apt purge lxd* lxc*
 
 apt install iftop iotop python3-distutils tor deb.torproject.org-keyring tor-geoipdb nyx
