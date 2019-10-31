cc3f70a206f3bda8a6ad688ce516be9b84c8caf5
root <root@tor001.janxb.de>
2019-10-31 14:14:19 +0100

    log reboot actions to file

--- cron_reboot.sh
+++ cron_reboot.sh
@@ -3,6 +3,8 @@ set -eu
 
 DIR=/root/tor-node-config
 
-git -C $DIR pull -q
+do-package-upgrades -y >> /var/log/tor/package-upgrades.log
+
+git -C $DIR pull >> /var/log/git-pull.log
 
 $DIR/generate_tor_config.sh
